b58a08b6c452028c799a55033a872588bd7cc3de
Arnau Ferreres <arnauferreres1@gmail.com>
2021-11-04 17:13:53 +0100

    .

--- Ejercicio de practica/Main.cpp
+++ Ejercicio de practica/Main.cpp
@@ -1,7 +1,48 @@
 #include <iostream> //libreria de la consola
+using namespace std;
 
 int main() 
 {
 	// << std::end1 = /n (salto de linea)
-	std::cout << "Hello world!!" << std::endl;
+	//char letra_A;
+	//char letra_B;
+	//char letra_C;
+	//std::cin >> letra_A;
+	//std::cin >> letra_B;
+	//std::cin >> letra_C;
+	//std::cout << "has escrito" << letra_A << std::endl;
+	//std::cout << "has escrito" << letra_B << std::endl;
+	//std::cout << "has escrito" << letra_C << std::endl;
+	/*int numero = 10;
+	std::cin >> numero;
+	cout << "has escrito" << numero << endl;
+	std::cout << "has escrito" << numero << std::endl;
+	system("CLS");*/
+	/*float numero_A = 2;
+	float numero_B = 3;
+	float numero_C = 4;
+	float respuesta = numero_C * numero_A * (numero_B - numero_C) / numero_B;
+	cout << respuesta;*/
+	int A = 0;
+	int B = 0;
+	cout << "Escribe el primer valor:" << endl;
+	cin >> A;
+	cout << "Escribe el primer valor:" << endl;
+	cin >> B;
+	cout << "Primer valor: " << A << endl;
+	cout << "Segundo valor: " << B << endl;
+	int resultado;
+	char operacion;
+	cout << "Que operacion queires hacer? (* o /)" << endl;
+	cin >> operacion;
+	if (operacion == '*')
+	{
+		resultado = A * B;
+	}
+	else if (operacion == '/')
+	{
+		resultado = A / B;
+	}
+	cout << "Resultado: " << resultado << endl;
+
 }
---
+++ ImprimirFotogramas/ImprimirFotogramas.cpp
@@ -0,0 +1,150 @@
+// ImprimirFotogramas.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+#define CONSOLE_HEIGHT 29
+#define CONSOLE_WIDTH 119
+
+
+void ImprimirPantalla();
+void RellenarMapa();
+void Inputs();
+void Start();
+void Logica();
+
+enum MAP_TILES {EMPTY = ' ', WALL = '#', POINT = '-' };
+enum USER_INPUTS {NONE, UP, DOWN, RIGHT, LEFT, QUIT};
+
+MAP_TILES ConsoleScreen[CONSOLE_HEIGHT][CONSOLE_WIDTH];
+char personaje = 'O';
+int personaje_x = 59;
+int personaje_y = 14;
+USER_INPUTS input = USER_INPUTS::NONE;
+bool run = true;
+
+
+int main(){
+
+    Start();
+    while (run)
+    {
+        Inputs();
+        Logica();
+        ImprimirPantalla();
+    }
+}
+void Start() {
+    
+        RellenarMapa();
+        ImprimirPantalla();
+    }
+
+    void RellenarMapa(){
+
+
+        for (int i = 0; i < CONSOLE_HEIGHT; i++)
+        {
+            for (int j = 0; j < CONSOLE_WIDTH; j++)
+            {
+                if (i == CONSOLE_HEIGHT - 1 || i == 0 || j == CONSOLE_WIDTH - 1 || j == 0)
+                {
+                    ConsoleScreen[i][j] = MAP_TILES::WALL;
+                }
+                else {
+                    ConsoleScreen[i][j] = MAP_TILES::EMPTY;
+                }
+            }
+        }
+    }
+    void Inputs() {
+        char input_raw;
+        cin >> input_raw;
+        switch (input_raw)
+        {
+            case 'W':
+            case 'w':
+                input = USER_INPUTS::UP;
+                break;
+            case 'S':
+            case 's':
+                input = USER_INPUTS::DOWN;
+                break;
+            case 'D':
+            case 'd':
+                input = USER_INPUTS::RIGHT;
+                break;
+            case 'A':
+            case 'a':
+                input = USER_INPUTS::LEFT;
+                break;
+            case 'Q':
+            case 'q':
+                run = false;
+                break;
+            default:
+                break;
+
+        }
+
+    }
+
+    void Logica() {
+        int personaje_y_new = personaje_y;
+        int personaje_x_new = personaje_x;
+        switch (input)
+        {
+        case UP:
+            personaje_y_new--;
+            break;
+        case DOWN:
+            personaje_y_new++;
+            break;
+        case RIGHT:
+            personaje_x_new++;
+            break;
+        case LEFT:
+            personaje_x_new--;
+            break;
+        case QUIT:
+            run = false;
+            break;
+        }
+        if (ConsoleScreen[personaje_y_new][personaje_x_new] == MAP_TILES::WALL) {
+            personaje_y_new = personaje_y;
+            personaje_x_new = personaje_x;
+        }
+        personaje_y = personaje_y_new;
+        personaje_x = personaje_x_new;
+    }
+
+    void ImprimirPantalla(){
+
+        system("CLS");
+
+    for (int i = 0; i < CONSOLE_HEIGHT; i++)
+    {
+        for (int j = 0; j < CONSOLE_WIDTH; j++)
+        {
+            if (personaje_x == j && personaje_y == i)
+            {
+                cout << personaje;
+            }
+            else {
+                cout << (char)ConsoleScreen[i][j];
+
+            }
+        }
+        cout << endl;
+    }
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
